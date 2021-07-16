59187701b91ad1cc4dea9efb700289c9d1954adb
Nathan <nathanraysheryak@gmail.com>
2021-07-16 06:36:50 -0400

    knex dev config + nodemon

---
+++ knexFile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      host : 'localhost',
+      database: 'cloud_note',
+      user:     'postgres',
+      password: '2345'
+    },
+    seeds: {
+      directory: './seeds/dev'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
