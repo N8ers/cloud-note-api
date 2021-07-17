32d0c62de699f5431c99e7b3543544c5b0081472
Nathan <nathanraysheryak@gmail.com>
2021-07-17 11:05:06 -0400

    setup eslint

--- knexFile.js
+++ knexFile.js
@@ -2,22 +2,22 @@
 
 module.exports = {
   development: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      host : 'localhost',
-      database: 'cloud_note',
-      user:     'postgres',
-      password: '2345'
+      host: "localhost",
+      database: "cloud_note",
+      user: "postgres",
+      password: "2345",
     },
     seeds: {
-      directory: './seeds/dev'
+      directory: "./seeds/dev",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+      tableName: "knex_migrations",
+    },
+  },
 };
