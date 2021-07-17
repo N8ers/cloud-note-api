e7724e1b10c0a2734f313b306cb9c76a6d866a07
Nathan <nathanraysheryak@gmail.com>
2021-07-17 11:04:38 -0400

    setup app entrance

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+
+const { development } = require("./knexFile");
+const knex = require("knex")(development);
+
+const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Nathan!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello Nathan!");
+});
 
-app.listen(port, () => {
-  console.log(`Hi Nathan! Server is running on http://localhost:${port} :)`)
-})
+async function init() {
+  // check that we can query db
+  const users = await knex.select(1).from("users").limit(1);
+
+  if (users && users.length) {
+    app.listen(port, () => {
+      console.log(
+        `Hi Nathan! Server is running on http://localhost:${port} :)`
+      );
+    });
+  } else {
+    console.log("connection to db failed, process exited");
+    process.exit(1);
+  }
+}
+
+init();
