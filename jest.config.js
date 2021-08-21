16077f4b8e452181be953d753a5b77f94db5761e
Nathan <nathanraysheryak@gmail.com>
2021-08-21 15:03:40 -0400

    get test db working!

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+const config = {
+  verbose: true,
+  globalSetup:
+    "<rootDir>/node_modules/@databases/pg-test/lib/jest/globalSetup.js",
+  globalTeardown:
+    "<rootDir>/node_modules/@databases/pg-test/lib/jest/globalTeardown.js",
+};
+
+module.exports = config;
