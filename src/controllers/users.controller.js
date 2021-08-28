8d599ce1c88ad198b02ea3bd07f28f6156237134
Nathan <nathanraysheryak@gmail.com>
2021-08-28 07:16:56 -0400

    post users + some safety checks

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,34 @@
+const knex = require("../../config/config");
+
+async function postUser(body) {
+  const { userName } = body;
+
+  if (!userName.length) {
+    return {
+      status: 400,
+      result: "Username is needed.",
+    };
+  }
+
+  let result;
+
+  try {
+    result = await knex("users")
+      .insert({
+        user_name: userName,
+      })
+      .returning("*");
+  } catch (error) {
+    return {
+      status: 400,
+      result: `There was a problem creating a new user: \n${error.message}`,
+    };
+  }
+
+  return {
+    result,
+    status: 200,
+  };
+}
+
+module.exports = { postUser };
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,16 @@
 const router = require("express").Router();
+// const knex = require("../../config/config");
 
 const { createGenericRoutes } = require("../helpers/genericRouteBuilder");
+const { postUser } = require("../controllers/users.controller");
 
 const genericRoutesToMake = ["getAll", "getById"];
 const tableToQuery = "users";
 createGenericRoutes(genericRoutesToMake, tableToQuery, router);
+
+router.post("/", async (req, res) => {
+  const { result, status } = await postUser(req.body);
+  res.status(status).send(result);
+});
 
 module.exports = router;
